2bdc5ad7d97aa1ada25f33c799bfd23fb668a414
unknown <korsh624@gmail.com>
2024-10-28 17:07:39 +0300

    add worck bot

--- 3.5CD/bot.py
+++ 3.5CD/bot.py
@@ -1,48 +1,25 @@
-import asyncio
+from random import randint
+from aiogram import types, Bot, Dispatcher,executor
 import logging
-import sys
-from io import BytesIO
-from os import getenv
+from PIL import Image
+from loaddata import catordog
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token='5077133016:AAFeAjz4GDOe_39siIkaenFULthrEQ07YLY')
+dp = Dispatcher(bot)
 
-from aiogram import Bot, Dispatcher, html, F
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-
-# Bot token can be obtained via https://t.me/BotFather
-TOKEN = '5077133016:AAFeAjz4GDOe_39siIkaenFULthrEQ07YLY'
-dp = Dispatcher()
+@dp.message_handler(commands='start')
+async def echo(message: types.Message):
+   await message.answer(message.from_user.id)
 
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+@dp.message_handler(content_types=["photo"])
+async def get_photo(message):
+    filname=str(randint(1,100))+'.jpg'
+    await message.photo[-1].download(destination_file=filname)
+    img = Image.open(filname)
+    answer=catordog(img)
+    await message.answer(answer)
 
 
-@dp.message(F.photo)
-async def get_photo(message: Message):
-    raw = await message.photo[0].download()
-    b = BytesIO()
-    b.write(raw.raw)
-    with open('testfile.jpg', 'wb') as f:
-        f.write(b.getvalue())
-
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.answer("Nice try!")
-
-
-
-
-async def main() -> None:
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+if __name__ == '__main__':
+    executor.start_polling(dp,skip_updates=True)
